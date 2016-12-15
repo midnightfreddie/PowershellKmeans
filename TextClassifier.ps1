14a4ae1a6dcb8985d2d5aff69e3836702deb295a
Jim Nelson <jim@midnightfreddie.com>
2016-12-15 01:13:45 -0600

    Featurize-String added

--- TextClassifier.ps1
+++ TextClassifier.ps1
@@ -23,3 +23,26 @@ function Get-NGrams {
         $String[$i..($i+$n-1)] -join ""
     }
 }
+
+function Featurize-String {
+    param (
+        [Parameter(Mandatory=$true)][string]$String,
+        $MaxDimensions = 1000,
+        $NGramMatch = '^\w+$'
+    )
+    $MD5 = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
+    $UTF8 = New-Object System.Text.UTF8Encoding
+    $NGrams = Get-NGrams $String |
+        Where-Object { $PSItem -match $NGramMatch }
+    $VectorHash = @{}
+    $NGrams | ForEach-Object {
+        $StringHash =  $MD5.ComputeHash($UTF8.GetBytes($PSItem))
+        # NOTE: Uint64 only holds the least-significant 64 bits of the md5 hash, but we're limiting dimensions so should be ok
+        $Key = [System.BitConverter]::ToUInt64($StringHash, 8) % $MaxDimensions
+        $VectorHash[$Key] += 1 / $NGrams.Count
+    }
+    $VectorHash.GetEnumerator() | ForEach-Object {
+        @($PSItem.Key, $PSItem.Value)
+    }
+}
+
